f1b6c99ecf808635630e60243684c36b0af06a75
Nikita <roziver@gmail.com>
2020-05-11 18:56:58 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -34,45 +35,40 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Text(
-                      '+7 999 423 11 34',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.teal,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Ubutu',
-                      ),
-                    )
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.white,
                 ),
               ),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+7 999 423 11 34',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Ubutu',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
                       Icons.email,
                       color: Colors.teal,
                     ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Text(
+                    title: Text(
                       'snaker@gmail.com',
                       style: TextStyle(
                         fontSize: 20.0,
@@ -80,10 +76,8 @@ class MyApp extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                         fontFamily: 'Ubuntu',
                       ),
-                    )
-                  ],
-                ),
-              )
+                    ),
+                  )),
             ],
           ),
         ),
